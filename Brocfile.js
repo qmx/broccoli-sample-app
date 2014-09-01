52d15b818213542d67987715f3eb57fb357fcb4a
Douglas Campos <qmx@qmx.me>
2014-09-01 01:12:50 -0300

    minify deps

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
 var concat = require('broccoli-concat');
+var uglifyJS = require('broccoli-uglify-js');
 
 
 var deps = pickFiles('bower_components', {
@@ -15,6 +16,8 @@ var concatenatedDeps = concat(deps, {
     ],
     outputFile: '/assets/deps.js'
 });
+
+concatenatedDeps = uglifyJS(concatenatedDeps);
 
 var tree = mergeTrees([concatenatedDeps, 'app', 'public']);
 module.exports = tree;
