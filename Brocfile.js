822dc7d460d76991afc0ee261d05aaafe3649c67
Douglas Campos <qmx@qmx.me>
2014-09-01 00:54:53 -0300

    let’s concatenate deps

--- Brocfile.js
+++ Brocfile.js
@@ -1,10 +1,20 @@
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
+var concat = require('broccoli-concat');
+
 
 var deps = pickFiles('bower_components', {
     srcDir: '/',
     destDir: '/deps'
 });
 
-var tree = mergeTrees([deps, 'app', 'public']);
+var concatenatedDeps = concat(deps, {
+    inputFiles: [
+        'deps/jquery/dist/jquery.js',
+        'deps/lodash/dist/lodash.js'
+    ],
+    outputFile: '/assets/deps.js'
+});
+
+var tree = mergeTrees([concatenatedDeps, 'app', 'public']);
 module.exports = tree;
